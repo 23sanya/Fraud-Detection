bb3781ccb09bbb0571b53062dd6f7debc25582b0
23sanya <sharmas.sanya97@gmail.com>
2022-07-28 23:12:09 +0530

    new algo defination is added to the branch

--- model_file.py
+++ model_file.py
@@ -1,1 +1,11 @@
 print("this is the new file model file whcih is made specifically for the new branch")
+
+def sum(a,b):
+    c=a+b
+    return c
+
+def model1():
+    return "model 1 algorithm"
+
+def model2():
+    return "model 2 algorithm"
